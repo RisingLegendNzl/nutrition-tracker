2894c8568b8456bc8c185ca854321e3c55aa068b
LEGEND <risinglegendnzl@gmail.com>
2025-09-15 14:42:21 +1000

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const mealPlan = {
+  Monday: {
+    Breakfast: {
+      calories: 1311, protein: 56, fibre: 19,
+      iron: 6.5, zinc: 7, calcium: 944,
+      vitaminC: 193, folate: 135, potassium: 3770
+    },
+    Lunch: {
+      calories: 1323, protein: 121, fibre: 28,
+      iron: 15, zinc: 20, calcium: 0,
+      vitaminC: 60, folate: 530, potassium: 3145
+    },
+    Dinner: {
+      calories: 935, protein: 72, fibre: 19,
+      iron: 6.8, zinc: 6, calcium: 0,
+      vitaminC: 63, folate: 410, potassium: 2660
+    }
+  }
+};
