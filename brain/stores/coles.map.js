e0abe59053232b60e72a6e5e513fc120f301a6cb
LEGEND <risinglegendnzl@gmail.com>
2025-09-18 15:44:34 +1000

    Update coles.map.js

--- brain/stores/coles.map.js
+++ brain/stores/coles.map.js
@@ -1,18 +1,16 @@
 // brain/stores/coles.map.js
-// Mapping from canonical food IDs -> Coles product info (URL, pack size, brand).
+// Auto-generated filtered map (only existing food IDs)
 export default {
-  food_rolled_oats: {
-    url: "https://www.coles.com.au/product/coles-rolled-oats-value-pack-1.8-kg-4972693",
-    pack_size_g: 1800,
-    brand: "Coles"
+  food_avocado: {
+    url: "https://www.coles.com.au/",
+    brand: "Produce"
   },
-  food_full_cream_milk: {
-    url: "https://www.coles.com.au/product/coles-dairy-full-cream-milk-1l-9052681",
-    pack_size_ml: 1000,
-    brand: "Coles"
+  food_banana: {
+    url: "https://www.coles.com.au/",
+    brand: "Produce"
   },
-  food_peanut_butter: {
-    url: "https://www.coles.com.au/product/coles-peanut-butter-smooth-500g-8100621",
+  food_beef_mince_5_lean: {
+    url: "https://www.coles.com.au/product/coles-no-added-hormone-beef-5-star-extra-trim-mince-500g-8112799",
     pack_size_g: 500,
     brand: "Coles"
   },
@@ -21,35 +19,40 @@ export default {
     pack_size_g: 1200,
     brand: "Coles"
   },
-  food_beef_mince_5_lean: {
-    url: "https://www.coles.com.au/product/coles-no-added-hormone-beef-5-star-extra-trim-mince-500g-8112799",
-    pack_size_g: 500,
+  food_frozen_mixed_vegetables: {
+    url: "https://www.coles.com.au/product/coles-simply-frozen-mixed-vegetables-1kg-7489339",
+    pack_size_g: 1000,
     brand: "Coles"
+  },
+  food_full_cream_milk: {
+    url: "https://www.coles.com.au/product/coles-dairy-full-cream-milk-1l-9052681",
+    pack_size_ml: 1000,
+    brand: "Coles"
+  },
+  food_greek_yogurt: {
+    url: "https://www.coles.com.au/",
+    brand: "Dairy"
   },
   food_lentils_canned_drained: {
     url: "https://www.coles.com.au/browse/pantry/canned-food-soups-noodles/canned-beans-legumes",
     pack_size_g: 400,
     brand: "Various"
   },
-  food_frozen_mixed_vegetables: {
-    url: "https://www.coles.com.au/product/coles-simply-frozen-mixed-vegetables-1kg-7489339",
-    pack_size_g: 1000,
-    brand: "Coles"
-  },
   food_olive_oil: {
     url: "https://www.coles.com.au/product/cobram-estate-extra-virgin-olive-oil-light-750ml-7452179",
     pack_size_ml: 750,
     brand: "Cobram Estate"
   },
-  food_banana: {
-    url: "https://www.coles.com.au/",
-    brand: "Produce"
+  food_peanut_butter: {
+    url: "https://www.coles.com.au/product/coles-peanut-butter-smooth-500g-8100621",
+    pack_size_g: 500,
+    brand: "Coles"
   },
-  food_sweet_potato: {
-    url: "https://www.coles.com.au/",
-    brand: "Produce"
+  food_peas: {
+    url: "https://www.coles.com.au/browse/frozen/frozen-vegetables",
+    brand: "Frozen"
   },
-  food_spinach: {
+  food_potatoes: {
     url: "https://www.coles.com.au/",
     brand: "Produce"
   },
@@ -57,19 +60,16 @@ export default {
     url: "https://www.coles.com.au/",
     brand: "Pantry rice"
   },
-  food_greek_yogurt: {
-    url: "https://www.coles.com.au/",
-    brand: "Dairy"
+  food_rolled_oats: {
+    url: "https://www.coles.com.au/product/coles-rolled-oats-value-pack-1.8-kg-4972693",
+    pack_size_g: 1800,
+    brand: "Coles"
   },
-  food_avocado: {
+  food_spinach: {
     url: "https://www.coles.com.au/",
     brand: "Produce"
   },
-  food_peas: {
-    url: "https://www.coles.com.au/browse/frozen/frozen-vegetables",
-    brand: "Frozen"
-  },
-  food_potatoes: {
+  food_sweet_potato: {
     url: "https://www.coles.com.au/",
     brand: "Produce"
   }
