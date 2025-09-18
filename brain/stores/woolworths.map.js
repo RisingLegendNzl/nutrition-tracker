c162ae845ab7df97d8239975f847ddd926bdf8bd
LEGEND <risinglegendnzl@gmail.com>
2025-09-18 12:58:33 +1000

    Create woolworths.map.js

---
+++ brain/stores/woolworths.map.js
@@ -0,0 +1,44 @@
+// brain/stores/woolworths.map.js
+// Mapping from canonical food IDs -> Woolworths product info.
+export default {
+  food_rolled_oats: {
+    url: "https://www.woolworths.com.au/shop/productdetails/321220/woolworths-rolled-traditional-oats",
+    pack_size_g: 750,
+    brand: "Woolworths"
+  },
+  food_full_cream_milk: {
+    url: "https://www.woolworths.com.au/shop/productdetails/151546/woolworths-full-cream-milk-milk",
+    pack_size_ml: 1000,
+    brand: "Woolworths"
+  },
+  food_peanut_butter: {
+    url: "https://www.woolworths.com.au/shop/productdetails/233922/woolworths-smooth-peanut-butter",
+    pack_size_g: 500,
+    brand: "Woolworths"
+  },
+  food_chicken_thigh_fillets: {
+    url: "https://www.woolworths.com.au/shop/productdetails/727461/woolworths-rspca-approved-chicken-thigh-fillets-skinless-tray",
+    pack_size_g: 1200,
+    brand: "Woolworths"
+  },
+  food_beef_mince_5_lean: {
+    url: "https://www.woolworths.com.au/shop/productdetails/577860/woolworths-heart-smart-extra-lean-beef-mince",
+    pack_size_g: 500,
+    brand: "Woolworths"
+  },
+  food_lentils_canned_drained: {
+    url: "https://www.woolworths.com.au/shop/productdetails/732195/edgell-brown-lentils",
+    pack_size_g: 400,
+    brand: "Edgell"
+  },
+  food_frozen_mixed_vegetables: {
+    url: "https://www.woolworths.com.au/shop/productdetails/768568/woolworths-mixed-vegetables",
+    pack_size_g: 1000,
+    brand: "Woolworths"
+  },
+  food_olive_oil: {
+    url: "https://www.woolworths.com.au/shop/productdetails/248084/woolworths-extra-virgin-olive-oil",
+    pack_size_ml: 500,
+    brand: "Woolworths"
+  }
+};
