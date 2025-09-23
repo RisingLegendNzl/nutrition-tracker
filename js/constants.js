26c79824bdee6c73d1c769a0ffcb414d6803ae51
LEGEND <risinglegendnzl@gmail.com>
2025-09-23 04:14:56 +0000

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,10 @@
+// js/constants.js
+// Centralized constants: localStorage keys & event names
+
+export const LS_KEYS = Object.freeze({
+  PLAN: 'NUTRIFY__PLAN'
+});
+
+export const EVENTS = Object.freeze({
+  PLAN_UPDATED: 'nutrify:planUpdated'
+});
