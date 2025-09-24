63361d94284ee20ac454e14b5b25badb8b290c82
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:49:40 +1000

    Create events.js

---
+++ js/events.js
@@ -0,0 +1,6 @@
+// Event definitions and helpers for the Nutrify application.
+// This module will dispatch and listen for custom events in later phases.
+
+const events = {};
+
+export default events;
