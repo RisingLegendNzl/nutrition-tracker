dc145e4fc69bb97601f20423e2d8e0c0d1af2674
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:51:09 +1000

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,4 @@
+// Simple client-side router for navigating between views in the Nutrify application.
+// The router will be implemented in later phases. This is just a placeholder.
+
+export default {};
