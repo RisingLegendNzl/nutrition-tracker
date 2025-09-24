710fb556cc634eab89f5f6e9973ef2556664e0b3
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:53:09 +1000

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,6 @@
+// Central state management for the Nutrify application.
+// In later phases, this module will coordinate application state and event handling.
+
+const state = {};
+
+export default state;
