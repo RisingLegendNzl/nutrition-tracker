dc628773eb694f65ef264961e0175113c4b48220
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:50:36 +1000

    Create validators.js

---
+++ js/validators.js
@@ -0,0 +1,10 @@
+// Validation functions for user input and application data.
+
+export function validateProfile(profile) {
+  // In later phases this will contain actual validation logic.
+  return true;
+}
+
+export default {
+  validateProfile,
+};
