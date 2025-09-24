9336694d51e4d9d964e313acb285f181bcda0091
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:50:15 +1000

    Create featureFlags.js

---
+++ js/featureFlags.js
@@ -0,0 +1,8 @@
+// Feature flag toggles for the Nutrify application.
+// Use these flags to enable or disable features during development.
+
+const featureFlags = {
+  // Example: 'enableHydration': false,
+};
+
+export default featureFlags;
