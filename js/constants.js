3b9639542f26d8a2b965398934b6ab06197e3859
LEGEND <risinglegendnzl@gmail.com>
2025-09-24 19:49:21 +1000

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+// Centralized constants for the Nutrify application.
+// Constants such as event names or localStorage keys will be defined here
+// in later phases.
+
+const constants = {};
+
+export default constants;
