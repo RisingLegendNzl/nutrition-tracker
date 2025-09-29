192a369b2e48fd556732f6b6f7a086ffb98f5fac
LEGEND <risinglegendnzl@gmail.com>
2025-09-29 16:09:54 +1000

    Update coles.map.js

--- brain/stores/coles.map.js
+++ brain/stores/coles.map.js
@@ -1,5 +1,69 @@
-// Mapping of food IDs to Coles store URLs.
+// Mapping of food IDs to Coles store search URLs (AU).
+// Using search URLs keeps us brand-agnostic while staying within stores/*.
 
-const colesMap = {};
+const colesMap = {
+  chicken_breast_raw:            "https://www.coles.com.au/search?q=chicken%20breast",
+  broccoli_florets:              "https://www.coles.com.au/search?q=broccoli%20florets",
+  carrot:                        "https://www.coles.com.au/search?q=carrot",
+  capsicum_red:                  "https://www.coles.com.au/search?q=red%20capsicum",
+  soy_sauce:                     "https://www.coles.com.au/search?q=soy%20sauce",
+  garlic:                        "https://www.coles.com.au/search?q=garlic",
+  ginger:                        "https://www.coles.com.au/search?q=ginger",
+  canola_or_rice_bran_oil:       "https://www.coles.com.au/search?q=canola%20oil%20rice%20bran%20oil",
+  rice_cooked:                   "https://www.coles.com.au/search?q=microwave%20white%20rice",
+
+  eggs:                          "https://www.coles.com.au/search?q=eggs",
+  bacon_rashers:                 "https://www.coles.com.au/search?q=bacon%20rashers",
+  canned_tomatoes:               "https://www.coles.com.au/search?q=canned%20tomatoes",
+  baked_beans:                   "https://www.coles.com.au/search?q=baked%20beans",
+  olive_oil:                     "https://www.coles.com.au/search?q=olive%20oil",
+  chilli_flakes:                 "https://www.coles.com.au/search?q=chilli%20flakes",
+  bread_sourdough:               "https://www.coles.com.au/search?q=sourdough%20bread",
+
+  onion_brown:                   "https://www.coles.com.au/search?q=brown%20onion",
+  potato:                        "https://www.coles.com.au/search?q=potato",
+  aubergine_eggplant:            "https://www.coles.com.au/search?q=eggplant",
+  button_mushrooms:              "https://www.coles.com.au/search?q=button%20mushrooms",
+  curry_paste:                   "https://www.coles.com.au/search?q=curry%20paste",
+  veg_stock:                     "https://www.coles.com.au/search?q=vegetable%20stock",
+  coconut_milk_light:            "https://www.coles.com.au/search?q=coconut%20milk%20light",
+  coriander_fresh:               "https://www.coles.com.au/search?q=coriander%20bunch",
+
+  tomatoes_roasted:              "https://www.coles.com.au/search?q=roasted%20tomatoes",
+  feta:                          "https://www.coles.com.au/search?q=feta",
+  bread_rye:                     "https://www.coles.com.au/search?q=rye%20bread",
+
+  rolled_oats_gluten_free:       "https://www.coles.com.au/search?q=gluten%20free%20rolled%20oats",
+  chia_seeds:                    "https://www.coles.com.au/search?q=chia%20seeds",
+  plant_milk_almond_unsweetened: "https://www.coles.com.au/search?q=almond%20milk%20unsweetened",
+  banana:                        "https://www.coles.com.au/search?q=banana",
+  berries_mixed:                 "https://www.coles.com.au/search?q=mixed%20berries",
+
+  potato_baking:                 "https://www.coles.com.au/search?q=baking%20potato",
+  canned_chickpeas_drained:      "https://www.coles.com.au/search?q=canned%20chickpeas",
+  tomato_passata:                "https://www.coles.com.au/search?q=tomato%20passata",
+  curry_powder:                  "https://www.coles.com.au/search?q=curry%20powder",
+  spinach:                       "https://www.coles.com.au/search?q=spinach",
+
+  salmon_fillet_raw:             "https://www.coles.com.au/search?q=salmon%20fillet",
+  asparagus:                     "https://www.coles.com.au/search?q=asparagus",
+  lemon:                         "https://www.coles.com.au/search?q=lemon",
+  salt:                          "https://www.coles.com.au/search?q=salt",
+  orange_or_lemon_juice:         "https://www.coles.com.au/search?q=lemon%20juice%20orange%20juice",
+
+  gf_plain_flour:                "https://www.coles.com.au/search?q=gluten%20free%20plain%20flour",
+  plant_milk:                    "https://www.coles.com.au/search?q=plant%20milk",
+  egg_or_substitute:             "https://www.coles.com.au/search?q=egg%20substitute%20or%20eggs",
+  maple_syrup:                   "https://www.coles.com.au/search?q=maple%20syrup",
+  blueberries:                   "https://www.coles.com.au/search?q=blueberries",
+
+  snow_peas:                     "https://www.coles.com.au/search?q=snow%20peas",
+  sweet_sour_sauce:              "https://www.coles.com.au/search?q=sweet%20and%20sour%20sauce",
+
+  gf_pancake_mix:                "https://www.coles.com.au/search?q=gluten%20free%20pancake%20mix",
+  milk_or_df_milk:               "https://www.coles.com.au/search?q=milk%20almond%20milk%20oat%20milk",
+  protein_powder_vanilla:        "https://www.coles.com.au/search?q=protein%20powder%20vanilla",
+  peanut_butter:                 "https://www.coles.com.au/search?q=peanut%20butter"
+};
 
 export default colesMap;
